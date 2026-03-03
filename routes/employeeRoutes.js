47352ae130405f335acd326a056da2a7dfc18c39
Diya Maheshwari <diyamaheshwari124@gmail.com>
2026-03-03 20:38:24 +0530

    Protected employee routes

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/employeeController");
 const { protect } = require("../middleware/authMiddleware");
 
-router.post("/", controller.createEmployee);
+router.post("/", protect, controller.createEmployee);
 router.get("/", controller.getEmployees);
 router.get("/search", controller.searchEmployee);
 router.get("/:id", controller.getEmployeeById);
