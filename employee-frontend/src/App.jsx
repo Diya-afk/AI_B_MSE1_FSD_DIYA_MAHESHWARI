14af449364e32075cf75ad7cee48c3952d196d9a
Diya Maheshwari <diyamaheshwari124@gmail.com>
2026-03-04 01:21:13 +0530

    ok

--- employee-frontend/src/App.jsx
+++ employee-frontend/src/App.jsx
@@ -8,11 +8,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/"
+          element={token ? <Navigate to="/dashboard" /> : <Login />}
+        />
+
         <Route
           path="/dashboard"
           element={token ? <Dashboard /> : <Navigate to="/" />}
         />
+
+        {/* Catch ALL unknown routes */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
