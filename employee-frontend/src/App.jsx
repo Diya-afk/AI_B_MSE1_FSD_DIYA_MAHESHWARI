357a0b327c2b79a34fa94a063e2e15efc13c41c1
Diya Maheshwari <diyamaheshwari124@gmail.com>
2026-03-03 23:28:14 +0530

    Removed employees route

--- employee-frontend/src/App.jsx
+++ employee-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
-import Employees from "./pages/employees"; // if you made one
 import Navbar from "./components/navbar";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/employees" element={<Employees />} />
       </Routes>
     </BrowserRouter>
   );
